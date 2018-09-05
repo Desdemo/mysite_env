59e8bc7f2a234d923bc3c33ab6769f92830c3474
Desdemo <1184788560@qq.com>
2018-09-05 22:31:29 +0800

    Initial commit

---
+++ user/views.py
@@ -0,0 +1,103 @@
+from django.shortcuts import redirect, render
+from django.contrib import auth
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.http import JsonResponse
+from user.forms import LoginForm, RegForm, ChangeNicknameForm, BindEmailForm
+from .models import Profile
+
+def login(request):
+    if request.method == 'POST':
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            user = login_form.cleaned_data['user']
+            auth.login(request, user)
+            return redirect(request.GET.get('from', reverse('home')))
+    else:
+        login_form = LoginForm()
+    context = {}
+    context['login_form'] = login_form
+    return render(request, 'user/login.html', context)
+
+def login_for_modal(request):
+    login_form = LoginForm(request.POST)
+    data = {}
+    if login_form.is_valid():
+        user = login_form.cleaned_data['user']
+        auth.login(request, user)
+        data['status'] = 'SUCCESS'
+    else:
+        data['status'] = 'ERROR'
+    return JsonResponse(data)
+
+def register(request):
+    if request.method == 'POST':
+        reg_form = RegForm(request.POST)
+        if reg_form.is_valid():
+            username = reg_form.cleaned_data['username']
+            email = reg_form.cleaned_data['email']
+            password = reg_form.cleaned_data['password']
+            #创建用户
+            user = User.objects.create_user(username, email, password)
+            user.save()
+            #登录用户
+            user = auth.authenticate(username=username, password=password)
+            auth.login(request, user)
+            return redirect(request.GET.get('from', reverse('home')))
+    else:
+        reg_form = RegForm()
+
+    context = {}
+    context['reg_form'] = reg_form
+    return render(request, 'user/register.html', context)
+
+def logout(request):
+    auth.logout(request)
+    return redirect(request.GET.get('from', reverse('home')))
+
+def user_info(request):
+    context = {}
+    return render(request, 'user/user_info.html', context)
+
+def change_nickname(request):
+    if request.method == 'POST':
+        form = ChangeNicknameForm(request.POST, user=request.user)
+        print(form)
+        if form.is_valid():
+            nickname_new = form.cleaned_data['nickname_new']
+            profile, created = Profile.objects.get_or_create(user=request.user)
+            profile.nickname = nickname_new
+            profile.save()
+            return redirect(request.GET.get('from', reverse('home')))
+
+    else:
+        form = ChangeNicknameForm()
+
+    context = {}
+    context['page_title'] = '修改昵称'
+    context['form_title'] = '修改昵称'
+    context['submit_text'] = '修改'
+    context['form'] = form
+    context['return_back_url'] = request.GET.get('from', reverse('home'))
+    return render(request, 'form.html', context)
+
+def bind_email(request):
+    if request.method == 'POST':
+        form = BindEmailForm(request.POST, user=request.user)
+        print(form)
+        if form.is_valid():
+            nickname_new = form.cleaned_data['nickname_new']
+            profile, created = Profile.objects.get_or_create(user=request.user)
+            profile.nickname = nickname_new
+            profile.save()
+            return redirect(request.GET.get('from', reverse('home')))
+    else:
+        form = BindEmailForm()
+
+    context = {}
+    context['page_title'] = '绑定邮箱'
+    context['form_title'] = '绑定邮箱'
+    context['submit_text'] = '绑定'
+    context['form'] = form
+    context['return_back_url'] = request.GET.get('from', reverse('home'))
+    return render(request, 'user/bind_email.html', context)
